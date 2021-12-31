e625c9c1f9e122b310fcb9b2b453b0a569e7f980
Alex Marmolejo <alex97marmolejo@gmail.com>
2021-12-31 02:16:34 -0500

    created routes to each component and added nav bar

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -1,11 +1,18 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.css';
 import App from './App';
+import Shop from './components/Shop';
+import Nav from './components/Nav';
+import Cart from './components/Cart';
 
 const RouteSwitch = () => {
   return (
     <BrowserRouter>
+      <Nav />
       <Routes>
         <Route path="/" element={<App />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/cart" element={<Cart />} />
       </Routes>
     </BrowserRouter>
   );
