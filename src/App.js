895a081236e78a501f135bbf0e99653ce4bf36ad
Alex Marmolejo <alex97marmolejo@gmail.com>
2022-01-04 00:06:14 -0500

    added jumbotron with header text and image

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-const App = () => {
+import homeImage from './images/nzxt-computers.avif';
+
+const App = (prop) => {
   return (
     <div className="App">
-      <h1>hello from the home page</h1>
+      <div className="jumbotron">
+        <header>
+          <h1 className="home-header">Welcome to Computer World.</h1>
+          <h1 className="home-header">Experience Power.</h1>
+          <p className="home-description">
+            Search for the perfect machine from a variety of retailers.
+          </p>
+        </header>
+        <img className="home-image" src={homeImage} alt="images of computers" />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
