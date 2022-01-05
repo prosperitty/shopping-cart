17bffdc3d040370e951c8b12df927ff37170663f
Alex Marmolejo <alex97marmolejo@gmail.com>
2022-01-04 22:15:19 -0500

    added page heading and list of dummy products

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,7 +1,51 @@
+import cpSupremeX from '../images/cyberpower-supreme-2.jpg'
+
 const Shop = () => {
   return (
     <div className="shop">
-      <h1>hello from the shop</h1>
+      <h1>Discover</h1>
+      <div className="products">
+        <div className="product-card">
+          <div className="product-image-container">
+            <img src={cpSupremeX} alt="cyberpower supreme x pc" className="product-image"/>
+          </div>
+          <div className="card-detail-container">
+            <h4 className="product-company">CyberpowerPC</h4>
+            <p className="product-name">supremeX</p>
+            <p className="product-price">$999</p>
+          </div>
+        </div>
+        <div className="product-card">
+          <div className="product-image-container">
+            <img src={cpSupremeX} alt="cyberpower supreme x pc" className="product-image"/>
+          </div>
+          <div className="card-detail-container">
+            <h4 className="product-company">CyberpowerPC</h4>
+            <p className="product-name">supremeX</p>
+            <p className="product-price">$999</p>
+          </div>
+        </div>
+        <div className="product-card">
+          <div className="product-image-container">
+            <img src={cpSupremeX} alt="cyberpower supreme x pc" className="product-image"/>
+          </div>
+          <div className="card-detail-container">
+            <h4 className="product-company">CyberpowerPC</h4>
+            <p className="product-name">supremeX</p>
+            <p className="product-price">$999</p>
+          </div>
+        </div>
+        <div className="product-card">
+          <div className="product-image-container">
+            <img src={cpSupremeX} alt="cyberpower supreme x pc" className="product-image"/>
+          </div>
+          <div className="card-detail-container">
+            <h4 className="product-company">CyberpowerPC</h4>
+            <p className="product-name">supremeX</p>
+            <p className="product-price">$999</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
