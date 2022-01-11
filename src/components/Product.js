da39c3a65a56693864545426090c858ecaf81083
Alex Marmolejo <alex97marmolejo@gmail.com>
2022-01-11 01:55:49 -0500

    added input which takes number and updates state when submitted

--- src/components/Product.js
+++ src/components/Product.js
@@ -7,7 +7,11 @@ const Product = (props) => {
       <div className="card-detail-container">
         <h4 className="product-company">{props.company}</h4>
         <p className="product-name">{props.name}</p>
-        <p className="product-price"><span>$</span>{props.price}</p>
+        <div>
+          <span className="product-price"><span>$</span>{props.price}</span>
+          <input name={props.id} type="number" min="0" onChange={props.handleChange} />
+          <button name={props.id} onClick={props.sendToCart}>add to cart</button>
+        </div>
       </div>
     </div>
   );
