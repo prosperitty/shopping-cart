94bb58c1518bef6add14c7db0b998115e760fee9
Alex Marmolejo <alex97marmolejo@gmail.com>
2022-01-11 01:48:54 -0500

    created cart quantity logic and passed events and state to components

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
 import './App.css';
 import App from './App';
 import Shop from './components/Shop';
@@ -6,12 +7,34 @@ import Nav from './components/Nav';
 import Cart from './components/Cart';
 
 const RouteSwitch = () => {
+  const [cart, setCart] = useState();
+
+  const [itemQuantity, setItemQuantity] = useState({});
+  const [totalQuantity, setTotalQuantity] = useState(0);
+  const [totalCharge, setTotalCharge] = useState({});
+
+  const handleChange = (event) => {
+    const name = event.target.name
+    setItemQuantity((prevState) => ({
+      ...prevState,
+      [name]: event.target.value,
+    }));
+    console.log(itemQuantity);
+  }
+
+  const addToCart = (event) => {
+    let index = parseInt(event.target.name);
+    // console.log(event.target.name);
+    setTotalQuantity((prevState) => (
+      prevState + parseInt(itemQuantity[index])
+    ));
+  }
   return (
     <BrowserRouter>
-      <Nav />
+      <Nav cartQuantity={totalQuantity} />
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/shop" element={<Shop />} />
+        <Route path="/shop" element={<Shop addToCart={addToCart} handleChange={handleChange} />} />
         <Route path="/cart" element={<Cart />} />
       </Routes>
     </BrowserRouter>
