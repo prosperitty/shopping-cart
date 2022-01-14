9bfcccf201716a73510b931cb053d4320437da07
Alex Marmolejo <alex97marmolejo@gmail.com>
2022-01-13 21:00:12 -0500

    added cart props and displayed data to page

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,9 +1,39 @@
-const Cart = () => {
+const Cart = (props) => {
+  const cartItems = props.cart.map((item) => (
+    <div className="cart-card" key={item.id}>
+      <div className="cart-image-container">
+        <img
+          src={item.src}
+          alt="cyberpower supreme x pc"
+          className="product-image"
+        />
+      </div>
+      <div className="cart-detail-container">
+        <h4 className="product-company">{item.company}</h4>
+        <p className="product-name">{item.name}</p>
+        <div className="cart-detail">
+          <p className="product-price">
+            <span>$</span>
+            {parseFloat(item.price)}
+          </p>
+          <p>
+            <span>Quantity:</span>
+            <span>{item.quantity}</span>
+          </p>
+        </div>
+      </div>
+    </div>
+  ));
+
   return (
     <div className="cart">
-      <h1>hello from the cart</h1>
+      {cartItems}
+      <div>
+        <h3>subtotal:{props.subtotal}</h3>
+        <h3>total:{props.subtotal}</h3>
+      </div>
     </div>
   );
-}
+};
 
 export default Cart;
